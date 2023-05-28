202c52deb7453986772f3b723204e4dd021b5b2d
Michael <30936296+mtan93@users.noreply.github.com>
2023-05-28 14:36:35 +0100

    Update webmin-install.sh

--- webmin-install.sh
+++ webmin-install.sh
@@ -1,6 +1,14 @@
+if [ "$(id -u)" -ne 0 ]; then
+    echo "${RED}Error:${NORMAL} \`setup-repos.sh\` script must be run as root!" >&2
+    exit 1
+fi
+
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-sudo curl https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh | sh
+sudo curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sudo sh setup-repos.sh
 
 sudo apt-get update
 sudo apt-get install webmin -y
+
+rm setup-repos.sh
