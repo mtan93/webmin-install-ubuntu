81eba9644f1a526b4babb0621d678a466fbfcbbf
Michael <30936296+mtan93@users.noreply.github.com>
2023-05-28 14:34:20 +0100

    Update webmin-install.sh

--- webmin-install.sh
+++ webmin-install.sh
@@ -1,7 +1,6 @@
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
-sh setup-repos.sh
+sudo curl https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh | sh
 
 sudo apt-get update
 sudo apt-get install webmin -y
